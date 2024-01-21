ea0a16b5ea39ef15c050efcf8b82c65a59f6b9c4
prabh8331 <prabh8331@gmail.com>
2024-01-21 17:18:38 +0000

    auto commit

--- git_manage.sh
+++ git_manage.sh
@@ -1,7 +1,7 @@
+# auto pull, add, commit and push
 # bash /opt/notebooks/git_manage.sh
-
 # git status
 git pull origin master
-# !git add .
-git commit -a -m "auto push"
+git add .
+git commit -m "auto commit"
 git push origin master
